ba428bb0e73dc30d5da83cd8653a06536f0d0a6b
Daniel McNamee <48935313+daniel-mcnamee-dev@users.noreply.github.com>
2026-03-17 15:13:32 +0000

    Update app.js

--- app.js
+++ app.js
@@ -224,6 +224,8 @@ app.get("/checkout", (req, res) => {
 
 
 // ------------ SERVER -----------
-app.listen(3000, () => {
-    console.log("CoffeeOverflow running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`CoffeeOverflow running on port ${PORT}`);
 });
